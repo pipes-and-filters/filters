8436c8733bd6516b9939710ce7caebb6061c0fb1
Jason Richard Smith <jasonrichardsmith@gmail.com>
2016-11-05 17:17:11 +0100

    added error comments

--- errors.go
+++ errors.go
@@ -2,16 +2,19 @@ package filters
 
 import "fmt"
 
+// Error is a filter error
 type Error struct {
 	command string
 	err     []byte
 }
 
+// Write implements Writer interface
 func (e *Error) Write(p []byte) (n int, err error) {
 	e.err = append(e.err, p...)
 	return len(p), nil
 }
 
+// Error implements Error interface
 func (e Error) Error() string {
 	return fmt.Sprintf("%v: %q", e.command, e.err)
 }
